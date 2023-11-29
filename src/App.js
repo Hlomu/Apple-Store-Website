5529ca916f0d5d323f710474198eb9eb58799b0e
Nnanana Madisha <nnananamadisha99@gmail.com>
2023-11-29 20:22:39 +0200

    Code added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux'; // Import Provider
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage'; // Import your homepage component
+import ProductDetails from './ProductDetails';
+import Bag from './Bag'; // Import your Bag component
+import { BagProvider } from './BagContext'; // Import your BagProvider
+import Checkout from './Checkout';
+import ChangeUserInfo from './ChangeUserInfo';
+import ChangePaymentInfo from './ChangePaymentInfo';
+import OrderSuccess from './OrderSuccess';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BagProvider>
+      <Router>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/Bag" element={<Bag/>}/>
+        <Route path="/Checkout" element={<Checkout/>}/>
+        <Route path="/ChangeUserInfo" element={<ChangeUserInfo/>}/>
+        <Route path="/ChangePaymentInfo" element={<ChangePaymentInfo/>}/>
+        <Route path="/OrderSuccess" element={<OrderSuccess/>}/>
+        </Routes>
+      </Router>
+    </BagProvider>
   );
 }
 
